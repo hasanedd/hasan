fd4dab01b4c1c3e10bff6aca5c24815b7b29815e
Sajjad Ranjbar <83742735+phantomf4321@users.noreply.github.com>
2022-08-07 16:29:46 +0430

    Update code.py

--- code.py
+++ code.py
@@ -36,7 +36,17 @@ dataset['Gender'].replace('Female',0, inplace=True)
 dataset['Gender'].replace('Male',1, inplace=True)
 
 x_train,x_test, y_train,y_test = train_test_split(dataset.drop('Weight',axis=1), dataset.Weight, test_size=0.2, random_state=101)
-
+"""
+XGBRegressor(base_score=0.5, booster='gbtree', colsample_bylevel=1,
+             colsample_bynode=1, colsample_bytree=1, enable_categorical=False,
+             gamma=0, gpu_id=-1, importance_type=None,
+             interaction_constraints='', learning_rate=0.300000012,
+             max_delta_step=0, max_depth=6, min_child_weight=1, missing=nan,
+             monotone_constraints='()', n_estimators=100, n_jobs=4,
+             num_parallel_tree=1, predictor='auto', random_state=0, reg_alpha=0,
+             reg_lambda=1, scale_pos_weight=1, subsample=1, tree_method='exact',
+             validate_parameters=1, verbosity=None)
+"""
 regressor = XGBRegressor(n_estimators=100)
 regressor.fit(x_train, y_train)
 
