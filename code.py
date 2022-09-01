b5f8c342522b8da2367a5943a0fe1cd51910fcb5
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2022-09-01 10:46:53 +0430

    Update code.py

--- code.py
+++ code.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
 import seaborn as sns 
-
 from xgboost import XGBRegressor
 from sklearn.metrics import mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
